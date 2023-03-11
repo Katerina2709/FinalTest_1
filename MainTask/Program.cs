879ba7ba1cff57938b44270069201e1e7b20b000
Olga19bon <slizkovaev@mail.ru>
2023-03-11 09:26:51 +0300

    Add input from Console

--- MainTask/Program.cs
+++ MainTask/Program.cs
@@ -8,15 +8,50 @@
 // ["1234", "1567", "-2", "computer science"] -> ["-2"]
 // ['Russia", "Denmark", "Kazan"] -> []
 
-string[] ArrayStart = {"hello", "2", "world", "-)"};
-string[] ArrayFinal = new string [2];
-for (int i = 0, k = 0; i < ArrayStart.Length; i++)
+// ввод данных с консоли
+int ConsoleInputInt (string message)
 {
-    if(ArrayStart[i].Length <= 3)
-    {
-      ArrayFinal [k] = ArrayStart[i];
-      k++;
-    }
+    Console.Write(message);
+    int amount = Convert.ToInt32(Console.ReadLine());
+    return amount;
 }
 
-Console.WriteLine ($"[\"{string.Join ("\", \"", ArrayStart)}\"] -> [\"{string.Join ("\", \"", ArrayFinal)}\"]");
+string [] ConsoleInputStrArr (int count)
+{
+  string[] array = new string [count]; 
+  for (int i = 0; i < count; i++)
+  {
+    Console.Write($"Введите {i + 1} элемент массива: ");
+    array [i] = Console.ReadLine();
+  }  
+  return array;
+}
+
+int elements = ConsoleInputInt ("Введите количество элементов массива: ");
+if (elements <= 0 )
+{
+    Console.Write("Ввели неверную длину массива");
+}
+else
+{
+    string[] ArrayStart = ConsoleInputStrArr (elements);
+    string[] ArrayFinal = new string [0];
+    for (int i = 0, k = 0; i < ArrayStart.Length; i++)
+    {
+        if(ArrayStart[i].Length <= 3)
+        {
+           Array.Resize (ref ArrayFinal, ArrayFinal.Length + 1);
+           ArrayFinal [k] = ArrayStart[i];
+           k++;
+        }
+    }
+
+    Console.WriteLine ($"[\"{string.Join ("\", \"", ArrayStart)}\"] -> [\"{string.Join ("\", \"", ArrayFinal)}\"]");
+}
+
+
+
+
+
+
+
